41331e4ced3256a865445f6b6027976f1b3f23b4
Ahiruneko-Chan <63516930+Bido67@users.noreply.github.com>
2023-01-21 14:05:32 -0500

    Update ThemeAccordingToClient.plugin.js

--- ThemeAccordingToClient.plugin.js
+++ ThemeAccordingToClient.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Theme according to client
  * @version 1.0.0
- * @description Use the same theme file for Stable, PTB and Canary but with different settings! The body will get a class according to its type!     Classes: Stable: .stable | PTB: .ptb | Canary: .canary
+ * @description Use the same theme file for Stable, PTB and Canary but with different settings! The body will get a class according to its type!     Classes: Stable: .stable | PTB: .ptb | Canary: .canary | Any: .anyclient
  * @author Duck-Chan
  * @authorId 598208193772126259
  * @source https://github.com/Bido67/BetterDiscord-ThemeAccordingToClient
@@ -19,6 +19,7 @@ module.exports = class ThemeAccordingToClientPlugin {
         } else if (document.domain == "canary.discord.com") {
             document.body.classList.add("canary");
         }
+        document.body.classList.add("anyclient");
     }
     stop() {}
 };
