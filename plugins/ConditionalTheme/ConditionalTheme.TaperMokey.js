0d842bb602a5342e9daded6dc97bd49c526d0450
Ahiruneko-Chan <63516930+Bido67@users.noreply.github.com>
2023-02-12 12:06:30 -0500

    Update ConditionalTheme.TaperMokey.js
    uwu

--- plugins/ConditionalTheme/ConditionalTheme.TaperMokey.js
+++ plugins/ConditionalTheme/ConditionalTheme.TaperMokey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discord Theme
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  Same as the plugin but for your browser :D
 // @author       Ahiruneko-chan#6669
 // @match       https://ptb.discord.com/*
@@ -16,27 +16,45 @@
     'use strict';
     var isInstalledAttrib = document.createAttribute("contiditionalplugininstalled");
         isInstalledAttrib.value = true;
-        document.querySelector("body").attributes.setNamedItem(isInstalledAttrib);
+        document.body.attributes.setNamedItem(isInstalledAttrib);
         var isclientattrib = document.createAttribute("native");
         isclientattrib.value = false;
-
-    if(document.domain == "discord.com"){
-        document.querySelector("body").classList.add("stable");
-    }
-    if(document.domain == "canary.discord.com"){
-        document.querySelector("body").classList.add("canary");
-    }
-    if(document.domain == "ptb.discord.com"){
-        document.querySelector("body").classList.add("ptb");
-    }
-    document.querySelector("body").classList.add("anyclient")
-    document.devFreeze = false;
-        document.querySelector("body").addEventListener('pointerdown', (event) => {
+        var releaseChannelattrib = document.createAttribute("releasechannel");
+        try{
+            var releaseChannel = DiscordNative.app.getReleaseChannel()
+            // Edit to attribue instead of class, upcoming feature for my theme
+            
+                releaseChannelattrib.value = releaseChannel;
+	            
+            // Compat for my unupdated theme
+                document.body.classList.add(releaseChannel);
+                isclientattrib.value = true; // because its using the Release channel function, its native discord client :3
+                console.log("Added using Release Channel Function")
+        }catch(err){
+            console.log("Couldn't use release channel, using URL & Assuming WebBrowser")
+            console.log(err)
+            if (document.domain == "discord.com") {
+                releaseChannelattrib.value = "stable";
+                document.body.classList.add("stable");
+            } else if (document.domain == "ptb.discord.com") {
+                releaseChannelattrib.value = "ptb";
+                document.body.classList.add("ptb");
+            } else if (document.domain == "canary.discord.com") {
+                releaseChannelattrib.value = "canary";
+                document.body.classList.add("canary");
+            }
+        }
+        document.body.attributes.setNamedItem(releaseChannelattrib);
+        document.body.attributes.setNamedItem(isclientattrib);
+        
+        document.body.classList.add("anyclient");
+        //Add event listener for touch / mouse / pen input
+        document.devFreeze = false;
+        document.body.addEventListener('pointerdown', (event) => {
             if(!document.devFreeze){
     	        var myshit = document.createAttribute("inputType");
 	            myshit.value = event.pointerType;
-	            document.querySelector("body").attributes.setNamedItem(myshit);
+	            document.body.attributes.setNamedItem(myshit);
             }
         })
-    document.querySelector("body").attributes.setNamedItem(isclientattrib);
 })();
