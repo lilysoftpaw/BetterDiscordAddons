0943690752e5902a5c1d2cc6cd63d2cf87a8c118
Ahiruneko-Chan <63516930+Bido67@users.noreply.github.com>
2023-03-02 14:19:20 -0500

    New Plugin!
    uwu

---
+++ plugins/BoxesForMessages/BoxesForMessages.TaperMokey.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         Boxes for messages
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Same as the plugin but for your browser :D
+// @author       Ahiruneko-chan#6669
+// @match       https://ptb.discord.com/*
+// @match       https://canary.discord.com/*
+// @match       https://discord.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
+// @grant        none
+// @run-at document-body
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.BoxesForMessages = function BoxesForMessages(speed = 500, loopEnabled = true, ignoreStyled = false, ignoreMode = 0) {
+        this.loopSettings = {
+            delay: speed,
+            loop: loopEnabled,
+        };
+        this.ignoreStyled = ignoreStyled;
+        this.ignoreMode = ignoreMode; /* 0: Fully Ignore Styled; 1 Ignore always but check on interval, 2: Don't ignore */
+        this.classes = {
+            start: "BFM-Start",
+            message: "BFM-Msg",
+            end: "BFM-End",
+            check: "BFM-Done",
+        };
+        this.style = () => {
+            var q = 0;
+            var messagesToStyle = document.querySelector(".scrollerInner-2PPAp2").querySelectorAll(`li>.cozyMessage-1DWF9U${this.ignoreStyled == false ? "" : ":not(." + this.classes.check + ")"}`);
+            for (var i = 0; i < messagesToStyle.length; i++) {
+                var isGroupStart = messagesToStyle[i].parentElement.querySelectorAll(".groupStart-3Mlgv1").length != 0;
+                if (!isGroupStart && i == messagesToStyle.length - 1) {
+                    try {
+                        messagesToStyle[i - 1].classList.remove(this.classes.end);
+                    } catch (e) {}
+                    messagesToStyle[i].classList.add(this.classes.end);
+                } else if (isGroupStart && i == messagesToStyle.length - 1) {
+                    messagesToStyle[i].classList.add(this.classes.end);
+                }
+                if (isGroupStart) {
+                    try {
+                        messagesToStyle[i - 1].classList.add(this.classes.end);
+                    } catch (err) {}
+                    messagesToStyle[i].classList.add(this.classes.start);
+                }
+                messagesToStyle[i].classList.add(this.classes.message);
+                messagesToStyle[i].classList.add(this.classes.check);
+
+                q++;
+            }
+            messagesToStyle = "";
+            console.log(q);
+            q = 0;
+        };
+        this.loop = () => {
+            if (this.loopSettings.loop) {
+                this.style();
+            }
+            setTimeout(this.loop, this.loopSettings.delay);
+        };
+        this.loop();
+    };
+    document.BFM = new document.BoxesForMessages();
+})();
---
+++ plugins/BoxesForMessages/README.md
@@ -0,0 +1,57 @@
+# BetterDiscord-BoxesForMessages
+
+## What's this?
+
+This plugin is made to add boxes around your messages! (Works in browser too!! Requires TamperMonkey)
+
+Its very simple to use and the code is kinda simple.
+
+## Dependencies
+- Betterdiscord if used in a client.
+- Tampermonkey if used in a browser.
+- Could also be loaded in a Nativefier App, you just need to edit the plugin file to run start(), or simply use the tapermokey script one.
+
+## Features: 
+
+ - Doesn't work when theme doesn't support it.
+ - Doesn't add CSS to the dom (This means it doesn't create the boxes if custom css / theme isn't made for it)
+
+## How to use it:
+
+Download the plugin and put it in your plugins folder.
+
+And 
+
+Add this to your custom css / theme.
+
+```css
+/* These variables are to edit the border styling, box margin, and border radius. This is made to be at the beginning of the theme file.*/
+:root {
+  /* Variables: */
+  --BFM-border-size: 5px;
+  --BFM-border-color: red;
+  --BFM-border-style: solid;
+  --BFM-border-radius: 15px;
+  --BFM-margin: 40px;
+}
+/* Add the following in its entierty. This is made to go in deep into the theme file */
+:root {
+  --BFM-border: var(--BFM-border-size) var(--BFM-border-style) var(--BFM-border-color);
+}
+.BFM-End{
+    border-bottom: var(--BFM-border);
+    border-bottom-left-radius: var(--BFM-border-radius);
+    border-bottom-right-radius: var(--BFM-border-radius);
+}
+.BFM-Start{
+    border-top: var(--BFM-border);
+    border-top-left-radius: var(--BFM-border-radius);
+    border-top-right-radius: var(--BFM-border-radius);
+}
+.BFM-Msg{
+    border-left: var(--BFM-border);
+    border-right: var(--BFM-border);
+    margin-left: var(--BFM-margin);
+    margin-right: var(--BFM-margin);
+}
+```
